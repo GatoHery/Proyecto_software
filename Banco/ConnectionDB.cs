9162a9b3f66043f903d45012cb567a9c0197dffc
RodrigoUngo <00075419@uca.edu.sv>
2022-11-11 15:33:36 -0600

    new feature: added database connection library and class :sparkles:

---
+++ Banco/ConnectionDB.cs
@@ -0,0 +1,41 @@
+﻿using System.Data;
+using Npgsql;
+
+namespace Banco;
+
+public static class ConnectionDB
+{
+    private static String host = "127.0.0.1",
+        database = "",
+        userID = "",
+        password = "";
+
+    private static string sConnection =
+        $"Server={host};Port=5432;User ID={userID};Password={password};Database={database};";
+
+
+    public static DataTable ExecuteQuery(string query)
+    {
+        NpgsqlConnection connection = new NpgsqlConnection(sConnection);
+        DataSet ds = new DataSet();
+
+        connection.Open();
+            
+        NpgsqlDataAdapter da = new NpgsqlDataAdapter(query, connection);
+        da.Fill(ds);
+            
+        connection.Close();
+
+        return ds.Tables[0];
+    }
+
+    public static void ExecuteNonQuery(string act)
+    {
+        NpgsqlConnection connection = new NpgsqlConnection(sConnection);
+            
+        connection.Open();
+        NpgsqlCommand command = new NpgsqlCommand(act, connection);
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+}
